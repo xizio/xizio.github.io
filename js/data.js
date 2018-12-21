57a7eae8106be4d39fa61de57e1bc97dea2e0ece
xizio <46047346+xizio@users.noreply.github.com>
2018-12-21 22:18:39 +0800

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,6 +1,18 @@
 function getList(){
 	return [
 		{
+			title: "我在小鱼已经赚了211.55元，一起来玩吧！",
+			content: "简单好玩别犹豫！新人100%有礼！",
+			image: "image/03.png",
+			href: "http://wx.xy599.com/share.php?id=19001627&act_source=invite"
+		},
+		{
+			title: "免费领5元，10元即可提现",
+			content: "提供大量的应用试玩，多种赚钱方式！",
+			image: "image/04.png",
+			href: "http://im.jubaopeng8888.com/jbph5/api/sharePage/13661417"
+		},
+		{
 			title: "每三分钟就可以完成一个任务即可获得0.8-2元的奖励",
 			content: "每三分钟就可以完成一个任务即可获得0.8-2元的奖励",
 			image: "image/01.png",
