f94aa168cd8dc61086f6bee1c20f3482e573fdb6
kotocean <1491413492@qq.com>
2018-12-21 21:54:05 +0800

    添加信息展示页面

---
+++ js/data.js
@@ -0,0 +1,16 @@
+function getList(){
+	return [
+		{
+			title: "每三分钟就可以完成一个任务即可获得0.8-2元的奖励",
+			content: "每三分钟就可以完成一个任务即可获得0.8-2元的奖励",
+			image: "image/01.png",
+			href: "http://www.mapzqq-com.com/static/share/share.html?friend_share=685692"
+		},
+		{
+			title: "懒猫试玩，把你的闲暇时间变成钱！",
+			content: "我在懒猫每天都能赚很多零花钱，你也一起来？",
+			image: "image/02.png",
+			href: "http://m.cattryapp.com/u/2841002.html?from=singlemessage"
+		}
+	]
+}
